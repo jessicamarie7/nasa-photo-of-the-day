47c38d92516d9302a7baa3de9497f2c58229684e
Jessica Manak <jessica8angels@gmail.com>
2021-10-06 12:39:20 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import { API_KEY, BASE_URL } from "./constants";
 
 function App() {
+
+  const [imageData, setImageData] = useState('')
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+  
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <header>
+        <h1>Remembering you today, Dad</h1>
+        <p>
+          <span role="img" aria-label='go!'>🚀</span>as I always do when looking at the stars you loved so much! <span role="img" aria-label='go!'>🚀</span>
+        </p>
+        {/* <Date /> */}
+      </header>
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'K2LxRe9mA6FsDElDT3fCpg6nyrdADbcrUc0e9b6m'
